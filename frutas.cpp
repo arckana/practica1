3a107d91b1b01795ecc885f5912449b844525765
arckana <32946572+arckana@users.noreply.github.com>
2017-10-23 11:00:45 -0500

    Add files via upload

---
+++ frutas.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+int main()
+{
+int a;
+cout<<"selecciona un opcion:\n";
+cout<<"opcion 1 Rojo\n";
+cout<<"opcion 2 Amarillo\n";
+cout<<"opcion 3 Morado\n";
+cin>>a;
+	switch(a)
+	{	
+		case 1: cout<<"Manzana\n";
+		
+		break;
+		case 2: cout<<"Platano\n";
+		break;
+		case 3: cout<<"Uva\n";
+		break;
+		
+	
+}
+ system("pause");
+return 0;
+}
